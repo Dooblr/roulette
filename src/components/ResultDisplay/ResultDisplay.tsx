5e1c01c80118e4b04a1069f71f818ff0bab3a2d5
Daniel <otherdaniel@gmail.com>
2024-12-30 22:57:02 -0800

    update

--- src/components/ResultDisplay/ResultDisplay.tsx
+++ src/components/ResultDisplay/ResultDisplay.tsx
@@ -8,11 +8,19 @@ function ResultDisplay() {
 
   return (
     <div className="result-display">
-      <div 
-        className="result-display__number"
-        style={{ color: currentNumber.color === 'black' ? 'white' : currentNumber.color }}
-      >
-        {currentNumber.value === -1 ? '00' : currentNumber.value}
+      <div className="result-display__container">
+        <div 
+          className="result-display__number"
+          style={{ color: currentNumber.color === 'black' ? 'white' : currentNumber.color }}
+        >
+          {currentNumber.value === -1 ? '00' : currentNumber.value}
+        </div>
+        <div 
+          className="result-display__color"
+          style={{ color: currentNumber.color === 'black' ? 'white' : currentNumber.color }}
+        >
+          {currentNumber.color.toUpperCase()}
+        </div>
       </div>
     </div>
   )
